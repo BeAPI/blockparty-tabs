fbeb43c4ad5b245205348acfeae92bdf31974707
Milan Ricoul <mricoul@beapi.fr>
2025-02-26 10:01:20 +0000

    Release version 1.0.2

---
+++ build/blockparty-tabs-nav-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'f2ef4f3df3967b4e6226');
