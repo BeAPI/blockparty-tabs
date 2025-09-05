640c49aabaa8190ea1650dad27e66168b0d2725c
n-langle <nlangle@beapi.fr>
2025-09-05 14:13:02 +0000

    Release version 1.0.3

---
+++ build/blockparty-tabs-nav-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '161897c16708cb9bf02a');
