c31bd2f29bf98c52b5f009bcb9daaca65cb0f099
mricoul <mricoul@beapi.fr>
2025-11-09 22:30:40 +0100

    feat: updates icons to tabs blocks
    Adds icons to the tabs, tabs-nav, tabs-panel, and tabs-panels blocks to improve user experience in the editor.
    Removes the icon property from the block.json files and adds it to the index.js files.

--- src/blockparty-tabs-nav/index.js
+++ src/blockparty-tabs-nav/index.js
@@ -2,8 +2,11 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
+import { navigation } from '@beapi/icons';
 
 registerBlockType( metadata.name, {
+	icon: navigation,
+
 	/**
 	 * @see ./edit.js
 	 */
--- src/blockparty-tabs-panels/index.js
+++ src/blockparty-tabs-panels/index.js
@@ -2,8 +2,10 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
+import { grid } from '@wordpress/icons';
 
 registerBlockType( metadata.name, {
+	icon: grid,
 	edit: Edit,
 	save,
 } );
