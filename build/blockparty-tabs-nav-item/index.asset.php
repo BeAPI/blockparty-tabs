f06931e3e664102f3ec1076976edc33e2968544a
Paolo Tesei <ptesei@beapi.fr>
2026-04-27 15:02:47 +0000

    Release version 1.1.3

---
+++ build/blockparty-tabs-nav-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '3aff3e7a3c3c99d839a9');
---
+++ build/blockparty-tabs-nav/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-primitives'), 'version' => 'ec9bf281ecfce0f01542');
