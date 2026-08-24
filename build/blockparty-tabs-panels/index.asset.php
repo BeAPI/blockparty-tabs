d62a8a554659af8420491d8fe8e06449c6ff6902
Milan Ricoul <mricoul@beapi.fr>
2026-08-24 14:04:52 +0000

    Release version 1.1.5

---
+++ build/blockparty-tabs-panels/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-primitives'), 'version' => '65af8bc5f8e279bb1974');
---
+++ build/blockparty-tabs/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5905234d50447d7fc2e4');
